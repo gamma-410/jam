31cd6f50228ec7e2b61d56155652f0be5f4ba144
やぎきち <moka.drip.web@gmail.com>
2021-11-13 17:29:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,29 +6,28 @@ import glob
 import markdown
 
 title = "jam - PythonSSG"
-fileName = glob.glob('content/*.md')
+fileName = glob.gl('content/*.md')
 
 # i = 回数, name = 拡張子ついた名前, htmlName = 拡張子ついた名前
 for (i, name, htmlName) in zip(range(len(fileName)), fileName, fileName):
 #変換処理
-    mdOpen = open(name, 'r')
+    mdOpen = open(name, 'r', encoding="utf-8")
     mdData = mdOpen.read()
-
     md = markdown.Markdown()  # Create an instance?
 
     formatMdData = md.convert(mdData)  # To convert md to html, use md.convert.
 
-    html = open('assets/base.html')  # Define the HTML template.
+    html = open('assets/base.html', 'r', encoding="utf-8")  # Define the HTML template.
     htmlData = html.read()
     formatData = htmlData.format(title, formatMdData)  # Put the title and formatted md into the template.
 
 #表示処理
     name = os.path.splitext(os.path.basename(htmlName))[0]
 
-    print("jam 🍓: ", name+".md", " >>> ", "Creating", name+".html")
+    print("jam: ", name+".md", " >>> ", "Creating", name+".html")
 
     htmlFileName = name+'.html'
-    writeHTMLFile = open("generate/"+htmlFileName, 'w')
+    writeHTMLFile = open("generate/"+htmlFileName, 'w', encoding="utf-8")
     writeHTMLFile.write(formatData)
     writeHTMLFile.close()
 
