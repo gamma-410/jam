ca0d7bbc10effa78284e36b94d50e327b6bf8137
gamma-410 <mail@gamma410.win>
2022-08-02 15:49:48 +0900

    ブログ機能を実装した

--- content/test.md
+++
@@ -1,4 +0,0 @@
-2021・10・23
-# welcome jam. 
-
-[GitHub: jam](https://github.com/moka-drip/jam)
--- main.py
+++ main.py
@@ -3,14 +3,27 @@
 
 import os
 import glob
+import datetime
 import markdown
 
-title = "jam - PythonSSG"
+title = "MyBlog"
 fileName = glob.glob('content/*.md')
+dt_now = datetime.datetime.now()
+date = dt_now.strftime('%Y・%m・%d')
 
 # i = 回数, name = 拡張子ついた名前, htmlName = 拡張子ついた名前
-for (i, name, htmlName) in zip(range(len(fileName)), fileName, fileName):
-#変換処理
+for (i, name, htmlName) in zip(range(len(fileName)), fileName, fileName): #
+
+# ブログ機能を追加しました！ 使用する場合はコメントを外してください！
+
+# # mdデータを準備
+#     indexMdOpen = open('assets/index.md', 'a', encoding='utf-8')
+#     name1 = os.path.splitext(os.path.basename(htmlName))[0]
+#     indexMdOpen.write('- [' + name1 + ' - ' + date + ']' + '(' + name1 + '.html)  \n')
+#     indexMdOpen.close()
+
+
+# 表示処理
     mdOpen = open(name, 'r', encoding="utf-8")
     mdData = mdOpen.read()
     md = markdown.Markdown()  # Create an instance?
@@ -21,7 +34,7 @@ for (i, name, htmlName) in zip(range(len(fileName)), fileName, fileName):
     htmlData = html.read()
     formatData = htmlData.format(title, formatMdData)  # Put the title and formatted md into the template.
 
-#表示処理
+# 表示処理
     name = os.path.splitext(os.path.basename(htmlName))[0]
 
     print("jam: ", name+".md", " >>> ", "Creating", name+".html")
@@ -30,4 +43,27 @@ for (i, name, htmlName) in zip(range(len(fileName)), fileName, fileName):
     writeHTMLFile = open("generate/"+htmlFileName, 'w', encoding="utf-8")
     writeHTMLFile.write(formatData)
     writeHTMLFile.close()
+   
 
+
+# ブログ機能を追加しました！ 使用する場合はコメントを外してください！
+
+# mdOpen = open('assets/index.md', 'r', encoding="utf-8")
+# mdData = mdOpen.read()
+# md = markdown.Markdown()  # Create an instance?
+
+# formatMdData = md.convert(mdData)  # To convert md to html, use md.convert.
+
+# html = open('assets/base.html', 'r', encoding="utf-8")  # Define the HTML template.
+# htmlData = html.read()
+# formatData = htmlData.format(title, formatMdData)  # Put the title and formatted md into the template.
+
+# # 表示処理
+# name = os.path.splitext(os.path.basename(htmlName))[0]
+
+# print("jam: ", "index.md", " >>> ", "Creating", "index.html")
+
+# htmlFileName = 'index.html'
+# writeHTMLFile = open("generate/"+htmlFileName, 'w', encoding="utf-8")
+# writeHTMLFile.write(formatData)
+# writeHTMLFile.close()
